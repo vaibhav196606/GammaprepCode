96046777c280c4584ffe2e75f8230240e0c955ec
vaibhav196606 <vaibhav199606@gmail.com>
2025-11-08 14:46:27 +0530

    Add Meta Pixel and Google Analytics to _app.js with proper tracking

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,75 @@
 import '@/styles/globals.css'
 import { AuthProvider } from '@/context/AuthContext'
+import Script from 'next/script'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Track page views on route change
+    const handleRouteChange = (url) => {
+      if (window.fbq) {
+        window.fbq('track', 'PageView')
+      }
+      if (window.gtag) {
+        window.gtag('config', 'G-0TSX7TMFJS', {
+          page_path: url,
+        })
+      }
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      {/* Meta Pixel Code */}
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '545956210757805');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-0TSX7TMFJS"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0TSX7TMFJS');
+          `,
+        }}
+      />
+
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
   )
 }
 
