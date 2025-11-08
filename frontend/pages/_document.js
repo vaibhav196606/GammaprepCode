d631f1bffcc915116ac5d1dcbacaee2fa6794f58
vaibhav196606 <vaibhav199606@gmail.com>
2025-11-08 14:46:07 +0530

    Move Meta Pixel and Google Analytics to _app.js for better detection and tracking

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,36 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0TSX7TMFJS"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-0TSX7TMFJS');
-            `,
-          }}
-        />
-
-        {/* Meta Pixel */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '545956210757805');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+        {/* Meta Pixel noscript fallback */}
         <noscript>
           <img
             height="1"
